d058934ef4fc80ae491c0abf30f6fc0bc1b75c32
Dom3n <domen.drovenik@student.um.si>
2024-05-04 17:53:01 +0200

    neki

--- js/index.js
+++ js/index.js
@@ -1,24 +1,59 @@
-function uploadFile() {
-  var formData = new FormData();
-  var fileField = document.getElementById("fileInput");
+// function uploadFile() {
+//   var formData = new FormData();
+//   var fileField = document.getElementById("fileInput");
 
-  formData.append("file", fileField.files[0]);
+//   formData.append("file", fileField.files[0]);
+
+//   fetch("http://localhost:3000/upload", {
+//     method: "POST",
+//     body: formData,
+//   })
+//     .then((response) => response.json())
+//     .then((result) => {
+//       console.log("Success:", result);
+//       document.getElementById("results").innerHTML =
+//         result.status === "found"
+//           ? "Vulnerabilities found:<br>" + result.vulnerabilities.join("<br>")
+//           : result.message;
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//       document.getElementById("results").innerHTML =
+//         "An error occurred while processing your file.";
+//     });
+// }
+function uploadFile() {
+  document.getElementById("codeDisplay").innerHTML = "";
+  const fileInput = document.getElementById("fileInput");
+  const formData = new FormData();
+  formData.append("file", fileInput.files[0]);
 
   fetch("http://localhost:3000/upload", {
     method: "POST",
     body: formData,
   })
     .then((response) => response.json())
-    .then((result) => {
-      console.log("Success:", result);
+    .then((data) => {
+      const count = data.vulnerabilities.length;
+      console.log(count);
+      //--------------------------------
       document.getElementById("results").innerHTML =
-        result.status === "found"
-          ? "Vulnerabilities found:<br>" + result.vulnerabilities.join("<br>")
-          : result.message;
+        "<div>Number of vulnerabilities: " +
+        count +
+        "</div><div>Lines with vulnerabilities: " +
+        "</div>";
+      //-------------------------------------------
+      const codeDisplay = document.getElementById("codeDisplay");
+      //   codeDisplay.style.padding = "2rem";
+      const lines = data.content.split("\n");
+      lines.forEach((line, index) => {
+        const lineElement = document.createElement("div");
+        lineElement.textContent = line;
+        if (data.vulnerabilities.includes(index + 1)) {
+          lineElement.className = "vulnerable";
+        }
+        codeDisplay.appendChild(lineElement);
+      });
     })
-    .catch((error) => {
-      console.error("Error:", error);
-      document.getElementById("results").innerHTML =
-        "An error occurred while processing your file.";
-    });
+    .catch((error) => console.error("Error:", error));
 }
